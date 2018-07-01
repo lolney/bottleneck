1059b419b2892d8ee4d8d868ad27f939c64152cf
Redkang <eolneybell@gmail.com>
2018-07-01 17:54:23 -0400

    stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,7 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
-import { BinaryTreeComponent } from '../src/react-app/BinaryTreeComponent';
+import BinaryTreeComponent from '../src/react-app/BinaryTreeComponent';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
