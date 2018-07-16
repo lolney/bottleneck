69aec7f180994ba3263beebb4c1db7e245743cd1
Luke Olney <lukeolney@gmail.com>
2018-07-16 02:04:26 -0700

    Add a PIXI shader demo

---
+++ stories/pixiDemo.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { Graphics, Filter } from 'pixi.js';
+
+/**
+ * This has pretty CPU high usage in the Chrome GPU process (~33%),
+ * with or without the animation
+ */
+class PixiShader extends React.Component {
+    componentDidMount() {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+
+        var app = new PIXI.Application();
+        document.getElementById('shader').appendChild(app.view);
+
+        var rect = new Graphics();
+        rect.drawRect(0, 0, width, height);
+
+        const shader = `
+            varying vec2 vTextureCoord;
+            varying vec4 vColor;
+
+            uniform sampler2D uSampler;
+            uniform vec4 uTextureClamp;
+            uniform vec4 uColor;
+            uniform vec2 resolution;
+            uniform float time;
+
+            float distFromCenter() {
+                return length(vec2(.5,.5) - vTextureCoord);
+            }
+
+            void main(void)
+            {
+                float dis = distFromCenter();
+                gl_FragColor = texture2D(uSampler, vTextureCoord);
+                gl_FragColor.r = abs(cos(time/1000.0 + 20.0*dis));
+                gl_FragColor.g = 0.0;
+                gl_FragColor.b = 0.0;
+            }
+        `;
+
+        let myFilter = new Filter(
+            null,
+            shader, // fragment shader
+            {
+                time: { value: 0.0 },
+                resolution: { value: [width, height] }
+            }
+        );
+
+        const fps = 60;
+        const interval = 1000 / 60;
+        window.setInterval(() => {
+            myFilter.uniforms.time += interval;
+        }, interval);
+        rect.filters = [myFilter];
+        app.stage.addChild(rect);
+    }
+
+    render() {
+        return <div id="shader" />;
+    }
+}
+
+storiesOf('PIXI', module).add('shader', () => <PixiShader />);
