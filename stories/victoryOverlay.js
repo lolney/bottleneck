b84143582d2a1fbee43b6660bd684de9ada3f377
Luke Olney <lukeolney@gmail.com>
2018-10-27 14:46:29 -0700

    Fix VictoryOverlay storybook

--- stories/victoryOverlay.js
+++ stories/victoryOverlay.js
@@ -9,14 +9,30 @@ import VictoryOverlay from '../src/react-app/VictoryOverlay.jsx';
 import '../src/react-app/CSS/VictoryOverlay.scss';
 
 export const socket = {
-    addEventListener: (event, callback) => {
-        window.setTimeout(() => 50);
-    },
-    emit: () => {}
+    create(eventName) {
+        return {
+            addEventListener: (event, callback) => {
+                window.setTimeout(
+                    () => (event == eventName ? callback() : null),
+                    1000
+                );
+            },
+            emit: () => {}
+        };
+    }
 };
 
 storiesOf('VictoryOverlay', module)
     .addDecorator(StorybookConsole)
-    .add('Victory Overlay', () => (
-        <VictoryOverlay openWindow={(code) => alert(code)} socket={socket} />
+    .add('Win Overlay', () => (
+        <VictoryOverlay
+            openWindow={(code) => alert(code)}
+            socket={socket.create('gameWin')}
+        />
+    ))
+    .add('Lose Overlay', () => (
+        <VictoryOverlay
+            openWindow={(code) => alert(code)}
+            socket={socket.create('gameLose')}
+        />
     ));
