2d326a6b2762d6e6a3dd6a97c0bc27eae7beea47
Luke Olney <lukeolney@gmail.com>
2018-11-23 21:40:42 -0800

    Cleanup matchmaking.jsx

--- src/react-app/matchmaking/Matchmaking.jsx
+++ src/react-app/matchmaking/Matchmaking.jsx
@@ -11,7 +11,6 @@ export default class Matchmaking extends React.Component {
                 <div className="matchmaking-box">
                     <div className="title-box">
                         <div className="image-container">
-                            {' '}
                             <img
                                 className="player-img"
                                 alt="player icon"
@@ -22,39 +21,38 @@ export default class Matchmaking extends React.Component {
                     </div>
 
                     <div className="body-box">
-                        <div className="btn-cont">
-                            {' '}
-                            <Button
-                                className="btn-1 hud-button"
-                                onClick={() => window.open('/?mode=practice')}
-                            >
-                                {' '}
-                                <img
-                                    className="btn-row"
-                                    alt="practice icon"
-                                    src="assets/noun_boxing.png"
-                                    width="50px"
-                                />
-                                <div className="btn-row">Practice</div>
-                            </Button>
-                        </div>
-                        <div className="btn-cont">
-                            <Button
-                                className="btn-2 hud-button"
-                                onClick={() => window.open('/?mode=vs')}
-                            >
-                                <img
-                                    className="btn-row"
-                                    alt="fight icon"
-                                    src="assets/noun_fighting.png"
-                                    width="50px"
-                                />
-                                <div className="btn-row">VS</div>
-                            </Button>
-                        </div>
+                        {[
+                            {
+                                title: 'Practice',
+                                src: 'assets/noun_boxing.png',
+                                url: '/?mode=practice'
+                            },
+                            {
+                                title: 'VS',
+                                src: 'assets/noun_fighting.png',
+                                url: '/?mode=vs'
+                            }
+                        ].map((config, i) => ModeButton(config, i))}
                     </div>
                 </div>
             </div>
         );
     }
 }
+
+const ModeButton = (config, i) => (
+    <div key={config.title} className="btn-cont">
+        <Button
+            className={`btn-${i + 1} hud-button`}
+            onClick={() => window.location.assign(config.url)}
+        >
+            <img
+                className="btn-row"
+                alt="practice icon"
+                src={config.src}
+                width="50px"
+            />
+            <div className="btn-row">{config.title}</div>
+        </Button>
+    </div>
+);
